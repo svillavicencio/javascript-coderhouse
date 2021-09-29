1e4830b7521d5abafa41f5fc360d7f2ccfc92444
svillavicencio <sergionicolasvillavicencio@gmail.com>
2021-09-29 00:35:48 -0300

    Desafio 1 mejorado

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ let script = function() {
     let number1 = parseInt(prompt(`Ingrese el primero numero a sumar`));
     let number2 = parseInt(prompt(`Ingrese el segundo numero a sumar`));
 
+    let result = number1+number2;
+
+    if(isNaN(result)){
+        alert("Ingresar dos valores numericos por favor!");
+        return;
+    }
+
     alert(number1+number2);
 }
 
